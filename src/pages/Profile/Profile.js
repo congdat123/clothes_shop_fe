96571782a1879f5f522789cb3cad2fe89b247ac0
congdat2106 <84704516+congdat123@users.noreply.github.com>
2023-04-04 16:24:20 +0700

    # 7 Profile

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -16,6 +16,9 @@ const cx = classNames.bind(styles);
 function Profile() {
     const { userName } = useParams();
     const [dataUser, setDataUser] = useState([]);
+    const [bills, setBills] = useState([]);
+    const [total, setTotal] = useState(0);
+
     useEffect(() => {
         axios
             .create({
@@ -28,21 +31,46 @@ function Profile() {
             });
     }, []);
 
+    useEffect(() => {
+        axios
+            .create({
+                baseURL: `https://localhost:44387/api/Bills/viaUserName?UserName=${userName}`,
+            })
+            .get()
+            .then((response) => {
+                setBills(response.data);
+                setTotal(response.data.reduce((accumulator, item) => accumulator + item.total, 0));
+            });
+    }, []);
+
     return (
         <div className={cx('wrapper')}>
             {dataUser.map((item) => (
                 <div className={cx('info-component')}>
                     <div className={cx('info')}>
                         <Image className={cx('info-content-img')} src={item.avatar} />
-                        <div className={cx('info-content')}>
-                            <p>{item.fullName}</p>
-                            <p>{item.userName}</p>
+                        <div className={cx('info-content-component')}>
+                            <p className={cx('info-name')}>{item.fullName}</p>
+                            <div>
+                                <p className={cx('info-rank')}>Bảng xếp hạng</p>
+                                <p className={cx('info-rank-current')}>
+                                    {bills.length >= 0 && bills.length < 5
+                                        ? `Thành viên mới`
+                                        : bills.length >= 5 && bills.length < 10
+                                        ? 'Thành viên bạc'
+                                        : 'Thành viên vàng'}
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <Tabs size="lg" defaultValue={0}>
-                        <TabList variant="soft" color="neutral">
-                            <Tab className={cx('item')}>Thông tin tài khoản của bạn</Tab>
-                            <Tab className={cx('item')}>Thông tin 2</Tab>
+                    <Tabs size="lg" defaultValue={0} className={cx('tabs')}>
+                        <TabList variant="soft" color="neutral" className={cx('tab-list')}>
+                            <Tab>
+                                <p className={cx('item-tab')}>Thông tin tài khoản của bạn</p>
+                            </Tab>
+                            <Tab>
+                                <p className={cx('item-tab')}>Thông tin mua hàng</p>
+                            </Tab>
                         </TabList>
                         <TabPanel value={0}>
                             <div>
@@ -81,7 +109,20 @@ function Profile() {
                                 <Button>Edit</Button>
                             </Link>
                         </TabPanel>
-                        <TabPanel value={1}>Thông tin 2</TabPanel>
+                        <TabPanel value={1}>
+                            <div className={cx('info')}>
+                                <div className={cx('info-title')}>Số đơn hàng đã mua:</div>
+                                <div className={cx('info-content')}>
+                                    <p>{bills.length}</p>
+                                </div>
+                            </div>
+                            <div className={cx('info')}>
+                                <div className={cx('info-title')}>Tổng tiền đã mua:</div>
+                                <div className={cx('info-content')}>
+                                    <p>{total} VNĐ</p>
+                                </div>
+                            </div>
+                        </TabPanel>
                     </Tabs>
                 </div>
             ))}
